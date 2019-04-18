0374e29ae27268319e30751fc2df90267f9e2944
Paul Keir <pkeir@users.noreply.github.com>
2019-04-18 16:43:40 +0100

    Fixed a typo.

--- curtains-tests.cpp
+++ curtains-tests.cpp
@@ -1,9 +1,9 @@
 // #define CURTAINS_N
-#include "curtains-tests.cpp"
+#include "curtains-tests.hpp"
 
-// g++              -std=c++17 tests.hpp
+// g++              -std=c++17 curtains-tests.cpp
 // ...OR 
-// g++ -DCURTAINS_N -std=c++17 tests.hpp
+// g++ -DCURTAINS_N -std=c++17 curtains-tests.cpp
 
 int main(int argc, char *argv[])
 {
